628279db1709aa87098958c73bc1e59d721166fb
zD0n <dedmap0526@gmail.com>
2025-10-04 16:19:13 +0700

    Testing New Tree

--- Forest/forest.gd
+++ Forest/forest.gd
@@ -3,8 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ForPath/env/Nature/Tree.force_rebuild_on_load
-	$ForPath/env/Building/Coops.force_rebuild_on_load
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
