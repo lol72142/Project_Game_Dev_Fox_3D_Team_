aef7a1931c86d23791196b31fac6c037a03d84a6
lol72142 <xXMOMMLG360Xx@gmail.com>
2025-10-05 15:23:42 +0700

    render opt and add obj

--- script/tree_tag.gd
+++ script/tree_tag.gd
@@ -2,15 +2,30 @@ extends StaticBody3D
 
 var player: CharacterBody3D
 var big_forest
+var find = false
 
 func _ready() -> void:
-	big_forest = get_parent().get_parent().get_parent()
+	var group_check = get_parent().get_parent().get_parent()
+	if not group_check.get_groups().is_empty():
+		if group_check.get_groups()[0] == 'textures':
+			big_forest = group_check
+			find = true
+			$CollisionShape3D.disabled = true
+			$CollisionShape3D.debug_fill = false
+	
+	
 
 func _process(delta: float) -> void:
-	
-	if big_forest.player_node == null or big_forest.player_node is CharacterBody3D:
-		player = big_forest.player_node
-		var distance = global_position.distance_to(player.global_position) <= 30.0
+	if find:
+		if big_forest.player_node == null or big_forest.player_node is CharacterBody3D:
+			player = big_forest.player_node
+			var distance_tree_player_texture = global_position.distance_to(player.global_position) 
 		
-		visible = distance
+			visible = distance_tree_player_texture <= 60.0
+			if distance_tree_player_texture <= 5:
+				$CollisionShape3D.disabled = false
+				$CollisionShape3D.debug_fill = false
+			else:
+				$CollisionShape3D.disabled = true
+				$CollisionShape3D.debug_fill = true
 	
