f26dced8722477387202417f2f278b3b0a801198
lol72142 <xXMOMMLG360Xx@gmail.com>
2025-10-05 19:02:42 +0700

    setting and main menu

--- script/db_manager.gd
+++ script/db_manager.gd
@@ -5,4 +5,9 @@ extends HSlider
 var bus_index
 
 func _ready() -> void:
-	
+	bus_index = AudioServer.get_bus_index(bus_name)
+
+
+func _on_value_changed(value: float) -> void:
+	var db = linear_to_db(value)
+	AudioServer.set_bus_volume_db(bus_index, db)
